f44b371baa39704af4b8c97a06f97f3313b8ec9b
Daviiidddddd <davidcaste2005@gmail.com>
2025-10-20 15:13:44 -0500

    Update user data in tests.sql

--- tests.sql
+++ tests.sql
@@ -1,6 +1,6 @@
 CREATE TABLE users (id INT, name VARCHAR(100), active BOOLEAN);
-INSERT INTO users (id, name, active) VALUES (1, 'Ana', TRUE);
+INSERT INTO users (id, name, active) VALUES (1, 'David', TRUE);
 SELECT * FROM users;
 SELECT id, name FROM users WHERE active = TRUE;
-UPDATE users SET name = 'Ana Maria' WHERE id = 1;
+UPDATE users SET name = 'David Castellanos' WHERE id = 1;
 DELETE FROM users WHERE id = 1;
